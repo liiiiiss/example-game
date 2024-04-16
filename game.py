23b39dedabf9f91fff64ec1111e9820410f5b5ae
liis <liis-mirell.palmiste@voco.ee~>
2024-04-16 12:48:37 +0300

    Add project basic file

---
+++ game.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode([800,500])
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.quit
+        
